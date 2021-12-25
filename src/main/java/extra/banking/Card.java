031470e686a19c5263c4b083277b6283cf4e7f53
okutnii <93587636+okutnii@users.noreply.github.com>
2021-12-25 10:56:32 +0200

    Card class upd

--- src/main/java/extra/banking/Card.java
+++ src/main/java/extra/banking/Card.java
@@ -3,20 +3,14 @@ package extra.banking;
 class Card {
     private static int idCounter;
     private final int id;
-    private final String bin;
-    private final String ai;
-    private final String checksum;
     private final String codeNumber;
     private final String pin;
 
-    private int balance;
+    private final int balance;
 
 
     public Card(String bin, String ai, String checksum, String pin) {
         this.pin = pin;
-        this.bin = bin;
-        this.ai = ai;
-        this.checksum = checksum;
         this.codeNumber = bin + ai + checksum;
         this.balance = 0;
 
@@ -25,9 +19,6 @@ class Card {
 
     public Card(String number, String pin, int balance,int id) {
         this.pin = pin;
-        this.bin = number.substring(0,6);
-        this.ai = number.substring(6,15);
-        this.checksum = number.substring(15);
         this.codeNumber = number;
         this.balance = balance;
 
